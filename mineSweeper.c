ad9a39040c52e06d2e976ad40352c88ec2c6f3fd
ParsaSam <parsasam@tr0dd.lan>
2018-12-07 00:08:21 +0330

    updated

--- mineSweeper.c
+++ mineSweeper.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
 #define M 14
 #define N 30
-#define mine 40
+#define Mine 40
 
 void cls();
 void defineTable(char table[M][N]);
-void randMine(int mines[mine][2]);
+void randMine(int mines[],int size);
 void printTable(char table[M][N]);
 void setFlag(char table[M][N] ,int ,int);
 void unflag(char table[M][N] ,int ,int );
+void sweeper(char table[M][N],int,int);
+int checker(int mines[],int ,int,int);
 
 
 int main(){
     char table[M][N];
     defineTable(table);
-
+    int mines[Mine];
+    randMine(mines,Mine);
     char cmd[3];
     char xY[3];
 
     while(1){
+        printTable(table);
         
-
         fgets(cmd, sizeof(cmd)*3, stdin);
         int y , x;
         
@@ -45,20 +50,39 @@ int main(){
             fgets(xY, sizeof(xY)*3, stdin);
             x= xY[0] - 48;
             y= xY[2] - 48;
+            unflag(table,x,y);
             break;
         }
-
-        printTable(table);
-        
+        cls();
+    
     }
     
     return 0;
 }
 
+void randMine(int mines[],int size){
+    int randomNum;
+    srand(time(NULL));
+    for (int i =0;i<size ;i++){
+        randomNum = rand()%(M*N);
+        for(int j=0;j<i;j++){
+            if(mines[j]==randomNum) randomNum =-1;
+        }
+        if(randomNum != -1) mines[i]= randomNum;
+    }
+}
 
+///Err!?//
+int checker(int mines[],int n ,int a,int b){
+    int num = b*30 + a;
+    for(int i=0;i<n;i++){
+        if(num == mines[i])   return 1;
+    }
+    return 0;
+}
 
 void cls(){
-    system("cls");
+    system("clear");
 }
 
 void defineTable(char table[M][N]){
@@ -85,4 +109,8 @@ void setFlag(char table[M][N] ,int x ,int y){
 
 void unflag(char table[M][N] ,int x ,int y){
     table[x][y] = '#';
+}
+
+void sweeper(char table[M][N],int x,int y){
+
 }
